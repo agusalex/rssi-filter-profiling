70efbb04d1cb63e931652a14c8fdeb478a1587e9
Agustin Alexander <agustin.c.alexander@gmail.com>
2021-04-06 16:14:08 -0300

    set adaptive mode to be main mode

--- main.py
+++ main.py
@@ -15,22 +15,24 @@ if __name__ == '__main__':
 
     _file = pd.read_csv(_filename)
     _signal = _file['rssi']
-    _signal_kalman = function_per_step(_file, kalman)
     _distance = _file['distance']
-
-    # Find Median, Find steps
-    _signal_median = function_per_step(_file, median)
+    # find steps
     _steps = np.array(find_steps(_distance.values, meters_per_step=step_meters))
+    # Apply median filter to raw data in discrete steps
+    _signal_median = function_per_step(_file, median)
+    # Apply kalman filter to raw data in discrete steps
+    _signal_kalman = function_per_step(_file, kalman)
 
-    # Fit Logarithmic curve of signal loss to median
-    # _C, _residual = find_coeficient(_steps, _signal_median)
-    c2, n, _residual2 = find_coeficient_adaptive(_steps, _signal_median)
-
-    _log_of_distance_discrete = log_fit_adaptive(_steps, c2, n)
-    _log_of_distance_raw = log_fit_adaptive(_distance * step_meters, c2, n)
+    # Fit Logarithmic curve of signal loss to median and find c and n
+    _C, _n, _residual = find_coeficient(_steps, _signal_median)
+    # Find the predicted y's for each of our steps
+    _log_of_distance_discrete = distance_to_rssi(_steps, _C, _n)
+    # Find the predicted y's for each of all our dataset of distances (non discrete)
+    _log_of_distance_raw = distance_to_rssi(_distance * step_meters, _C, _n)
+    # Inverse of logarithmic function to visualize distance over signal median, make it lineal
+    _distance_infered_median = rssi_to_distance(_signal_median, _C, _n)
     # Inverse of logarithmic function to visualize distance over signal, make it lineal
-   # _distance_infered = rssi_to_distance(_signal_kalman, c2)
-   # _distance_infered_median = rssi_to_distance(_signal_median, c2)
+    _distance_infered = rssi_to_distance(_signal, _C, _n)
 
     # Graph it All!
 
@@ -41,19 +43,21 @@ if __name__ == '__main__':
     plot_signals([_signal_kalman, _log_of_distance_raw], [_filename, 'log_regression'], xlabel="Meters",
                  ylabel="Signal",
                  title="Signal vs Distance C=" +
-                       str(c2) + " N= " + str(n) + " R%= " + str(_residual2),
+                       str(_C) + " N= " + str(_n) + " R%= " + str(_residual),
                  xi=_distance * step_meters)
 
     plot_signals([_signal_median, _log_of_distance_discrete], [_filename, 'log_regression'], title="Signal Median vs "
-                 "Distance  C=" + str(c2) + " N= " + str(n) + " R%= " + str(_residual2),xi=_steps)
+                                                                                                   "Distance  C=" + str(
+        _C) + " N= " + str(_n) + " R%= " + str(_residual), xi=_steps)
 
-   # plot_signals([_distance_infered,kalman(_distance_infered), rssi_to_distance(_log_of_distance_raw, _C)], [_filename, "kalman",'log_regression'],
-   #              xlabel="Step Measurement", ylabel="Predicted Distance",
-   #              title="Inverse log, C=" +
-   #                    str(_C) + " R%= " + str(_residual),
-   #              xi=_distance)
+    plot_signals([_distance_infered, rssi_to_distance(_log_of_distance_raw, _C, _n)],
+                 [_filename, 'log_regression'],
+                 xlabel="Step Measurement", ylabel="Predicted Distance",
+                 title="Inverse log, C=" + str(
+        _C) + " N= " + str(_n) + " R%= " + str(_residual),
+                 xi=_distance)
 
-    # plot_signals([_distance_infered_median, rssi_to_distance(_log_of_distance_discrete, _C)],
-    #             [_filename, "log_regression"], xlabel="Step Measurement",
-    #             ylabel="Distance", title="Inverse Median C=" +
-    #                                      str(_C) + " R%= " + str(_residual))
+    plot_signals([_distance_infered_median, rssi_to_distance(_log_of_distance_discrete, _C, _n)],
+                 [_filename, "log_regression"], xlabel="Step Measurement",
+                 ylabel="Distance", title="Inverse Median C=" + str(
+        _C) + " N= " + str(_n) + " R%= " + str(_residual))

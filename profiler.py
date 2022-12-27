18062c108c100e094c3d2b8064620eff536fd019
Agustin Alexander <agustin.c.alexander@gmail.com>
2022-12-27 19:10:33 -0300

    wip

--- profiler.py
+++ profiler.py
@@ -12,7 +12,7 @@ def func_log(x, n, a):
 
 def signal_profiling(_filename, graph_or_not):
     # open file and read RSSI signal
-    step_meters = 1
+    step_meters = 4.5
     # for original experiment 4.5 for simulation 1:1 and 2.5 for inferred walking speed latter experiments
     _signal, _distance, file = prepare_signal(_filename)
 
@@ -29,33 +29,34 @@ def signal_profiling(_filename, graph_or_not):
     # Find the predicted y's for each of all our dataset of distances (non discrete)
     _log_of_distance_raw = distance_to_rssi(_distance * step_meters, _A, _n)
     # Inverse of logarithmic function to visualize distance over signal median, make it lineal
-    _distance_infered_median = rssi_to_distance(signal_mean, _A, _n)
+    _distance_infered_mean = rssi_to_distance(signal_mean, _A, _n)
     # Inverse of logarithmic function to visualize distance over signal, make it lineal
     _distance_infered = rssi_to_distance(_signal, _A, _n)
     # Graph it all!!
     if graph_or_not:
         plot_signals([_signal], [filename, 'Signal Kalman'], xlabel="Meters",
                      ylabel="Signal",
-                     title="Signal as RSSI A=" +
-                           str(round(_A)) + " R%= " + str(
-                         round(_residual)),
-                     xi=_distance)
+                     title="RSSI vs Distance",
+                     xi=_distance*step_meters)
         plot_signals([signal_mean, _log_of_distance_discrete], [filename, 'log_regression'],
-                     title="Signal Mean"
-                           f"vs Distance  "
+                     title="RSSI Mean and Log fit vs Distance "
                            f"A= {str(round(_A))}"
                            f" N= {str(round(_n))}"
                            f" R%={str(round(_residual))}",
                      xlabel="Meters",
-                     ylabel="Signal Median",
+                     ylabel="Signal Mean",
                      xi=_steps)
         plot_signals([_distance_infered, rssi_to_distance(_log_of_distance_raw, _A, _n)],
                      [filename, 'log_regression'],
                      xlabel="Step Measurement", ylabel="Predicted Distance",
-                     title="Inverse log, A=" + str(round(_A)) + " R%= " + str(round(_residual)), xi=_distance)
-        plot_signals([_distance_infered_median, rssi_to_distance(_log_of_distance_discrete, _A, _n)],
+                     title="Inverse log, "
+                           f"A= {str(round(_A))}"
+                           f" N= {str(round(_n))}"
+                           f" R%={str(round(_residual))}"
+                     , xi=_distance)
+        plot_signals([_distance_infered_mean, rssi_to_distance(_log_of_distance_discrete, _A, _n)],
                      [filename, "log_regression"], xlabel="Step Measurement",
-                     ylabel="Distance", title="Inverse Log Median A=" + str(round(
+                     ylabel="Distance", title="Inverse Log Mean A=" + str(round(
                 _A)) + f" N= {str(round(_n))}" + " R%= " + str(round(_residual)))
     log_canonical = rssi_to_distance(range(1, 50), _A, _n)
     return signal_mean, _steps, _log_of_distance_discrete, _A, _n, _residual, log_canonical
@@ -65,7 +66,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Filtering strategies for rssi time series')
     parser.add_argument('--file', nargs='?', help='data filename',
-                        default='data/simulation/simulation.csv')
+                        default='data/Device1.csv')
     parser.add_argument('--verbose', nargs='?', help='data filename',
                         default=True)
     args = parser.parse_args()
@@ -73,24 +74,23 @@ if __name__ == '__main__':
     logs = []
     logs_canonical = []
     for filename in filenames:
-        signal_median, _steps, log_of_distance_discrete, _A, _n, _residual, log_canonical = signal_profiling(
+        signal_mean, _steps, log_of_distance_discrete, _A, _n, _residual, log_canonical = signal_profiling(
             filename, args.verbose)
         logs.append(log_of_distance_discrete)
         logs_canonical.append(log_canonical)
-        plot_signals([signal_median, log_of_distance_discrete], [filename, 'log_regression'],
-                     title=f"Signal Median "
-                           f"vs Distance  "
+        plot_signals([signal_mean, log_of_distance_discrete], [filename, 'log_regression'],
+                     title=f"RSSI Mean and Log fit vs Distance  "
                            f"A= {str(round(_A))}"
                            f" N= {str(round(_n))}"
                            f" R%={str(round(_residual))}",
                      xlabel="Meters",
-                     ylabel="Signal Median",
+                     ylabel="Signal Mean",
                      xi=_steps)
 
     if len(filenames) > 1:
         plot_signals(logs, labels=filenames, title="Logs applied to each distance captured",
                      xlabel="Meters",
-                     ylabel="Signal Median",
+                     ylabel="Signal Mean",
                      xi=_steps
                      )
 
